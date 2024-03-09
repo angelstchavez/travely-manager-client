669e9d1dd0ab6d582df4f10a2f04f35c3ca5c707
angelstchavez <angelstchavez@gmail.com>
2024-03-09 12:57:18 -0500

    feat: pudate project

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,9 +1,20 @@
+import Header from "@/components/header";
+import HeaderMobile from "@/components/header-mobile";
+import MarginWidthWrapper from "@/components/margin-width-wrapper";
+import PageWrapper from "@/components/page-wrapper";
 import SideNav from "@/components/side-nav";
-import Link from "next/link";
 
-function Page() {
+function Page({ children }: { children: React.ReactNode }) {
   return (
     <div>
+      <SideNav />
+      <main className="flex-1">
+        <MarginWidthWrapper>
+          <Header />
+          <HeaderMobile />
+          <PageWrapper>{children}</PageWrapper>
+        </MarginWidthWrapper>
+      </main>
     </div>
   );
 }
