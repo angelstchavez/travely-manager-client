c870e901db9a33b4d5812c4806ee003e28628f7f
angelstchavez <angelstchavez@gmail.com>
2024-03-18 12:08:21 -0500

    feat: update model form

---
+++ src/components/models/model-form.tsx
@@ -0,0 +1,186 @@
+import React, { useState, useEffect } from "react";
+import Cookies from "js-cookie";
+
+interface Brand {
+  id: number;
+  name: string;
+}
+
+function ModelForm() {
+  const [brands, setBrands] = useState<Brand[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const cookieValue = decodeURIComponent(Cookies.get("authTokens") || "");
+        const cookieData = JSON.parse(cookieValue);
+        const token = cookieData.data.token;
+
+        if (!token) {
+          throw new Error("No se encontró el token en el cookie.");
+        }
+
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/car-brand/get-all`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${token}`,
+              Accept: "application/json",
+            },
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error("Error al obtener las marcas de vehículos.");
+        }
+
+        const responseData = await response.json();
+
+        if (!responseData.success || !responseData.data) {
+          throw new Error("La respuesta no contiene datos válidos.");
+        }
+
+        setBrands(responseData.data);
+      } catch (error: any) {
+        setError(error.message as string);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <section className="border rounded p-4 my-4 bg-white">
+      <h2 className="text-lg font-semibold">Gestor de Modelos de Vehículos</h2>
+      {/* Inputs adicionales */}
+      <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <div>
+          <label
+            htmlFor="category"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Nombre
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="w-full pl-3 pr-10 mt-1 border-gray-300 focus:outline-nonesm:text-sm rounded-mdml-1 relative inline-flex items-center space-x-2 px-4 py-2 border text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+            placeholder="Ingrese el nombre del modelo"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="category"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Categoría
+          </label>
+          <select
+            id="category"
+            name="category"
+            className="w-full pl-3 pr-10 mt-1 border-gray-300 focus:outline-nonesm:text-sm rounded-mdml-1 relative inline-flex items-center space-x-2 px-4 py-2 border text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+          >
+            <option value="">Seleccione</option>
+            <option value="compacto">Compacto</option>
+            <option value="sedán">Sedán</option>
+            <option value="SUV">SUV</option>
+            <option value="autobús">Autobús</option>
+            <option value="mini bus">Mini Bus</option>
+
+            {/* Opciones de categoría */}
+          </select>
+        </div>
+        <div>
+          <label
+            htmlFor="fuelType"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Tipo de Gasolina
+          </label>
+          <select
+            id="fuelType"
+            name="fuelType"
+            className="w-full pl-3 pr-10 mt-1 border-gray-300 focus:outline-nonesm:text-sm rounded-mdml-1 relative inline-flex items-center space-x-2 px-4 py-2 border text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+          >
+            <option value="">Seleccione</option>
+            <option value="gasolina regular">Gasolina Regular</option>
+            <option value="gasolina premium">Gasolina Premium</option>
+            <option value="diésel">Diésel</option>
+            <option value="etanol">Etanol</option>
+            <option value="gas natural">Gas Natural</option>
+            {/* Opciones de tipo de gasolina */}
+          </select>
+        </div>
+        <div>
+          <label
+            htmlFor="seatingCapacity"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Capacidad de Asientos
+          </label>
+          <input
+            type="text"
+            id="seatingCapacity"
+            name="seatingCapacity"
+            className="w-full pl-3 pr-10 mt-1 border-gray-300 focus:outline-nonesm:text-sm rounded-mdml-1 relative inline-flex items-center space-x-2 px-4 py-2 border text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+            placeholder="Ingrese la cantidad de asientos"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="transmissionType"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Tipo de Transmisión
+          </label>
+          <select
+            id="transmissionType"
+            name="transmissionType"
+            className="w-full pl-3 pr-10 mt-1 border-gray-300 focus:outline-nonesm:text-sm rounded-mdml-1 relative inline-flex items-center space-x-2 px-4 py-2 border text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+          >
+            <option value="">Seleccione</option>
+            <option value="manual">Manual</option>
+            <option value="automática">Automática</option>
+            {/* Opciones de tipo de transmisión */}
+          </select>
+        </div>
+        <div>
+          <label
+            htmlFor="brand"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Marca
+          </label>
+          <select
+            id="brand"
+            name="brand"
+            className="w-full pl-3 pr-10 mt-1 border-gray-300 focus:outline-nonesm:text-sm rounded-mdml-1 relative inline-flex items-center space-x-2 px-4 py-2 border text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+          >
+            <option value="">Seleccione</option>
+            {brands.map((brand) => (
+              <option key={brand.id} value={brand.name}>
+                {brand.name}
+              </option>
+            ))}
+            {/* Opciones de marca */}
+          </select>
+        </div>
+      </div>
+      <div className="mt-4 flex flex-col sm:flex-row sm:items-center justify-end">
+        <div className="relative flex-grow flex items-center">
+          <button
+            type="button"
+            className="ml-1 relative inline-flex items-center space-x-2 px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+          >
+            <span>Crear</span>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ModelForm;
---
+++ src/components/models/model-table.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ModelTable() {
+  return (
+    <section className="border rounded p-4 my-4 bg-white">
+      <h2 className="text-lg font-semibold">Lista de Modelos de Vehículos</h2>
+      {/* Aquí irá la tabla */}
+    </section>
+  );
+}
+
+export default ModelTable;
