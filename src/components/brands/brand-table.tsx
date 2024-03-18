bcc0cd82dd1546d3599fdf520394596b2352a5f5
angelstchavez <angelstchavez@gmail.com>
2024-03-18 01:39:26 -0500

    feat: update brand table

--- src/components/brands/brand-table.tsx
+++ src/components/brands/brand-table.tsx
@@ -1,28 +1,24 @@
 import React, { useState, useEffect } from "react";
 import ErrorModal from "../modals/error-modal";
 import Cookies from "js-cookie";
+import ConfirmationModal from "../modals/confirmation-modal";
 
 interface Brand {
   id: number;
   name: string;
-  // Agrega más propiedades según tus datos
 }
 
 function BrandTable(): JSX.Element {
   const [brands, setBrands] = useState<Brand[]>([]);
   const [error, setError] = useState<string>("");
+  const [brandToDelete, setBrandToDelete] = useState<Brand | null>(null);
 
   useEffect(() => {
     const fetchData = async (): Promise<void> => {
       try {
-        // Obtener el valor del cookie y decodificarlo
         const cookieValue = decodeURIComponent(Cookies.get("authTokens") || "");
-
-        // Convertir el valor del cookie a objeto JSON
         const cookieData: { data: { token?: string } } =
           JSON.parse(cookieValue);
-
-        // Obtener el token del objeto JSON
         const token = cookieData.data.token;
 
         if (!token) {
@@ -59,6 +55,53 @@ function BrandTable(): JSX.Element {
     fetchData();
   }, []);
 
+  const handleDeleteConfirmation = async () => {
+    if (brandToDelete) {
+      try {
+        const cookieValue = decodeURIComponent(Cookies.get("authTokens") || "");
+        const cookieData: { data: { token?: string } } =
+          JSON.parse(cookieValue);
+        const token = cookieData.data.token;
+
+        if (!token) {
+          throw new Error("No se encontró el token en el cookie.");
+        }
+
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/car-brand/delete?carBrandId=${brandToDelete.id}`,
+          {
+            method: "DELETE",
+            headers: {
+              Authorization: `Bearer ${token}`,
+              Accept: "application/json",
+            },
+          }
+        );
+
+        if (response.ok) {
+          const updatedBrands = brands.filter(
+            (brand) => brand.id !== brandToDelete.id
+          );
+          setBrands(updatedBrands);
+        } else {
+          throw new Error("Error al eliminar la marca de vehículo.");
+        }
+      } catch (error: any) {
+        setError(error.message);
+      } finally {
+        setBrandToDelete(null);
+      }
+    }
+  };
+
+  const handleDeleteCancel = () => {
+    setBrandToDelete(null);
+  };
+
+  const handleDelete = (brand: Brand) => {
+    setBrandToDelete(brand);
+  };
+
   return (
     <section className="border rounded p-4 my-4 bg-white">
       <h2 className="text-lg font-semibold">Lista de Marcas de Vehículos</h2>
@@ -73,7 +116,12 @@ function BrandTable(): JSX.Element {
               >
                 Nombre
               </th>
-              {/* Agrega más columnas según tus datos */}
+              <th
+                scope="col"
+                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+              >
+                Eliminar
+              </th>
             </tr>
           </thead>
           <tbody className="bg-white divide-y divide-gray-200">
@@ -82,12 +130,26 @@ function BrandTable(): JSX.Element {
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
                   {brand.name}
                 </td>
-                {/* Agrega más celdas según tus datos */}
+                <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                  <button
+                    className="text-red-600 hover:text-red-900"
+                    onClick={() => handleDelete(brand)}
+                  >
+                    Eliminar
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
+      {brandToDelete && (
+        <ConfirmationModal
+          processText={`eliminar la marca de vehículo "${brandToDelete.name}"`}
+          onAccept={handleDeleteConfirmation}
+          onCancel={handleDeleteCancel}
+        />
+      )}
     </section>
   );
 }
