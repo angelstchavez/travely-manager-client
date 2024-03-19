d343f81e7c975428c708908059749b4b741cfa99
angelstchavez <angelstchavez@gmail.com>
2024-03-18 19:08:49 -0500

    feat: update table models

--- src/components/models/model-form.tsx
+++ src/components/models/model-form.tsx
@@ -8,6 +8,14 @@ interface Brand {
 
 function ModelForm() {
   const [brands, setBrands] = useState<Brand[]>([]);
+  const [formData, setFormData] = useState({
+    name: "",
+    category: "",
+    fuelType: "",
+    seatingCapacity: "",
+    transmissionType: "",
+    brandId: "",
+  });
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
@@ -51,6 +59,57 @@ function ModelForm() {
     fetchData();
   }, []);
 
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const cookieValue = decodeURIComponent(Cookies.get("authTokens") || "");
+      const cookieData = JSON.parse(cookieValue);
+      const token = cookieData.data.token;
+
+      if (!token) {
+        throw new Error("No se encontró el token en el cookie.");
+      }
+
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/car-model/create`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+            Accept: "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Error al crear el modelo de vehículo.");
+      }
+
+      const responseData = await response.json();
+
+      if (!responseData.success) {
+        throw new Error(
+          responseData.message || "Error al crear el modelo de vehículo."
+        );
+      }
+
+      // Éxito al crear el modelo de vehículo
+      // Lógica adicional según sea necesario, como redirigir a una página diferente
+    } catch (error: any) {
+      setError(error.message as string);
+    }
+  };
+
   return (
     <section className="border rounded p-4 my-4 bg-white">
       <h2 className="text-lg font-semibold">Gestor de Modelos de Vehículos</h2>
@@ -58,7 +117,7 @@ function ModelForm() {
       <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 gap-4">
         <div>
           <label
-            htmlFor="category"
+            htmlFor="name"
             className="block text-sm font-medium text-gray-700"
           >
             Nombre
@@ -67,8 +126,11 @@ function ModelForm() {
             type="text"
             id="name"
             name="name"
-            className="w-full pl-3 pr-10 mt-1 border-gray-300 focus:outline-nonesm:text-sm rounded-mdml-1 relative inline-flex items-center space-x-2 px-4 py-2 border text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+            className="w-full pl-3 pr-10 mt-1 border-gray-300 focus:outline-none sm:text-sm rounded-mdml-1 relative inline-flex items-center space-x-2 px-4 py-2 border text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
             placeholder="Ingrese el nombre del modelo"
+            value={formData.name}
+            onChange={handleInputChange}
+            required
           />
         </div>
         <div>
@@ -81,15 +143,17 @@ function ModelForm() {
           <select
             id="category"
             name="category"
-            className="w-full pl-3 pr-10 mt-1 border-gray-300 focus:outline-nonesm:text-sm rounded-mdml-1 relative inline-flex items-center space-x-2 px-4 py-2 border text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+            className="w-full pl-3 pr-10 mt-1 border-gray-300 focus:outline-none sm:text-sm rounded-mdml-1 relative inline-flex items-center space-x-2 px-4 py-2 border text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+            value={formData.category}
+            onChange={handleInputChange}
+            required
           >
             <option value="">Seleccione</option>
-            <option value="compacto">Compacto</option>
-            <option value="sedán">Sedán</option>
-            <option value="SUV">SUV</option>
-            <option value="autobús">Autobús</option>
-            <option value="mini bus">Mini Bus</option>
-
+            <option value="Autobús">Autobús</option>
+            <option value="Automóvil">Automóvil</option>
+            <option value="Camioneta">Camioneta</option>
+            <option value="Camión">Camión</option>
+            <option value="Furgoneta">Furgoneta</option>
             {/* Opciones de categoría */}
           </select>
         </div>
@@ -103,14 +167,16 @@ function ModelForm() {
           <select
             id="fuelType"
             name="fuelType"
-            className="w-full pl-3 pr-10 mt-1 border-gray-300 focus:outline-nonesm:text-sm rounded-mdml-1 relative inline-flex items-center space-x-2 px-4 py-2 border text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+            className="w-full pl-3 pr-10 mt-1 border-gray-300 focus:outline-none sm:text-sm rounded-mdml-1 relative inline-flex items-center space-x-2 px-4 py-2 border text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+            value={formData.fuelType}
+            onChange={handleInputChange}
+            required
           >
             <option value="">Seleccione</option>
-            <option value="gasolina regular">Gasolina Regular</option>
-            <option value="gasolina premium">Gasolina Premium</option>
-            <option value="diésel">Diésel</option>
-            <option value="etanol">Etanol</option>
-            <option value="gas natural">Gas Natural</option>
+            <option value="Gasolina">Gasolina</option>
+            <option value="Diésel">Diésel</option>
+            <option value="Eléctrico">Eléctrico</option>
+            <option value="Híbrido">Híbrido</option>
             {/* Opciones de tipo de gasolina */}
           </select>
         </div>
@@ -125,8 +191,11 @@ function ModelForm() {
             type="text"
             id="seatingCapacity"
             name="seatingCapacity"
-            className="w-full pl-3 pr-10 mt-1 border-gray-300 focus:outline-nonesm:text-sm rounded-mdml-1 relative inline-flex items-center space-x-2 px-4 py-2 border text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+            className="w-full pl-3 pr-10 mt-1 border-gray-300 focus:outline-none sm:text-sm rounded-mdml-1 relative inline-flex items-center space-x-2 px-4 py-2 border text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
             placeholder="Ingrese la cantidad de asientos"
+            value={formData.seatingCapacity}
+            onChange={handleInputChange}
+            required
           />
         </div>
         <div>
@@ -139,29 +208,36 @@ function ModelForm() {
           <select
             id="transmissionType"
             name="transmissionType"
-            className="w-full pl-3 pr-10 mt-1 border-gray-300 focus:outline-nonesm:text-sm rounded-mdml-1 relative inline-flex items-center space-x-2 px-4 py-2 border text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+            className="w-full pl-3 pr-10 mt-1 border-gray-300 focus:outline-none sm:text-sm rounded-mdml-1 relative inline-flex items-center space-x-2 px-4 py-2 border text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+            value={formData.transmissionType}
+            onChange={handleInputChange}
+            required
           >
             <option value="">Seleccione</option>
-            <option value="manual">Manual</option>
-            <option value="automática">Automática</option>
+            <option value="Manual">Manual</option>
+            <option value="Automático">Automático</option>
+            <option value="Semi-automático">Semi-automático</option>
             {/* Opciones de tipo de transmisión */}
           </select>
         </div>
         <div>
           <label
-            htmlFor="brand"
+            htmlFor="brandId"
             className="block text-sm font-medium text-gray-700"
           >
             Marca
           </label>
           <select
-            id="brand"
-            name="brand"
-            className="w-full pl-3 pr-10 mt-1 border-gray-300 focus:outline-nonesm:text-sm rounded-mdml-1 relative inline-flex items-center space-x-2 px-4 py-2 border text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+            id="brandId"
+            name="brandId"
+            className="w-full pl-3 pr-10 mt-1 border-gray-300 focus:outline-none sm:text-sm rounded-mdml-1 relative inline-flex items-center space-x-2 px-4 py-2 border text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+            value={formData.brandId}
+            onChange={handleInputChange}
+            required
           >
             <option value="">Seleccione</option>
             {brands.map((brand) => (
-              <option key={brand.id} value={brand.name}>
+              <option key={brand.id} value={brand.id}>
                 {brand.name}
               </option>
             ))}
@@ -174,6 +250,7 @@ function ModelForm() {
           <button
             type="button"
             className="ml-1 relative inline-flex items-center space-x-2 px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+            onClick={handleSubmit}
           >
             <span>Crear</span>
           </button>
--- src/components/models/model-table.tsx
+++ src/components/models/model-table.tsx
@@ -1,12 +1,190 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ErrorModal from "../modals/error-modal";
+import ConfirmationModal from "../modals/confirmation-modal";
+import Pagination from "../pagination";
+import Cookies from "js-cookie";
 
-function ModelTable() {
+interface CarModel {
+  id: number;
+  name: string;
+  category: string;
+  fuelType: string;
+  seatingCapacity: number;
+  transmissionType: string;
+  carBrand: {
+    id: number;
+    name: string;
+  };
+}
+
+const CarModelList: React.FC = () => {
+  const [carModels, setCarModels] = useState<CarModel[]>([]);
+  const [error, setError] = useState<string>("");
+  const [carModelToDelete, setCarModelToDelete] = useState<CarModel | null>(
+    null
+  );
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [itemsPerPage] = useState<number>(10);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const cookieValue = decodeURIComponent(Cookies.get("authTokens") || "");
+        const cookieData: { data: { token?: string } } =
+          JSON.parse(cookieValue);
+        const token = cookieData.data.token;
+
+        if (!token) {
+          throw new Error("No se encontró el token en el cookie.");
+        }
+
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/car-model/get-all?page=${currentPage}&perPage=${itemsPerPage}`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${token}`,
+              Accept: "application/json",
+            },
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error("Error al obtener los modelos de vehículos.");
+        }
+
+        const responseData = await response.json();
+
+        if (!responseData.success || !responseData.data) {
+          throw new Error("La respuesta no contiene datos válidos.");
+        }
+
+        setCarModels(responseData.data);
+      } catch (error: any) {
+        setError(error.message);
+      }
+    };
+
+    fetchData();
+  }, [currentPage, itemsPerPage]);
+
+  const handleDeleteConfirmation = async () => {
+    if (carModelToDelete) {
+      try {
+        const cookieValue = decodeURIComponent(Cookies.get("authTokens") || "");
+        const cookieData: { data: { token?: string } } =
+          JSON.parse(cookieValue);
+        const token = cookieData.data.token;
+
+        if (!token) {
+          throw new Error("No se encontró el token en el cookie.");
+        }
+
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/car-model/delete?carModelId=${carModelToDelete.id}`,
+          {
+            method: "DELETE",
+            headers: {
+              Authorization: `Bearer ${token}`,
+              Accept: "application/json",
+            },
+          }
+        );
+
+        if (response.ok) {
+          const updatedCarModels = carModels.filter(
+            (carModel) => carModel.id !== carModelToDelete.id
+          );
+          setCarModels(updatedCarModels);
+        } else {
+          throw new Error("Error al eliminar el modelo de vehículo.");
+        }
+      } catch (error: any) {
+        setError(error.message);
+      } finally {
+        setCarModelToDelete(null);
+      }
+    }
+  };
+
+  const handleDeleteCancel = () => {
+    setCarModelToDelete(null);
+  };
+
+  const handleDelete = (carModel: CarModel) => {
+    setCarModelToDelete(carModel);
+  };
+
+  const totalPages = Math.ceil(carModels.length / itemsPerPage);
+
   return (
     <section className="border rounded p-4 my-4 bg-white">
       <h2 className="text-lg font-semibold">Lista de Modelos de Vehículos</h2>
-      {/* Aquí irá la tabla */}
+      {error && <ErrorModal errorDescription={error} />}
+      <div className="overflow-x-auto">
+        <table className="min-w-full divide-y divide-gray-200">
+          <thead className="bg-gray-50">
+            <tr>
+              <th className="table-cell w-1/5 md:w-auto lg:w-1/5 text-left">Nombre</th>
+              <th className="table-cell w-1/5 md:w-auto lg:w-1/5 text-left">Categoría</th>
+              <th className="table-cell w-1/5 md:w-auto lg:w-1/5 text-left">Marca</th>
+              <th className="table-cell w-1/5 md:w-auto lg:w-1/5 text-left">Gasolina</th>
+              <th className="table-cell w-1/5 md:w-auto lg:w-1/5 text-left">Asientos</th>
+              <th className="table-cell w-1/5 md:w-auto lg:w-1/5 text-left">
+                Transmisión
+              </th>
+              <th className="w-1/6 md:w-auto lg:w-1/6">Eliminar</th>
+            </tr>
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200">
+            {carModels.map((carModel) => (
+              <tr key={carModel.id} className="hover:bg-gray-100">
+                <td className="px-5 py-4 md:whitespace-nowrap text-sm font-medium text-gray-900 text-left">
+                  {carModel.name}
+                </td>
+                <td className="px-5 py-4 md:whitespace-nowrap text-sm font-medium text-gray-900 text-left">
+                  {carModel.category}
+                </td>
+                <td className="px-5 py-4 md:whitespace-nowrap text-sm font-medium text-gray-900 text-left">
+                  {carModel.carBrand.name}
+                </td>
+                <td className="px-5 py-4 md:whitespace-nowrap text-sm font-medium text-gray-900 text-left">
+                  {carModel.fuelType}
+                </td>
+                <td className="px-5 py-4 md:whitespace-nowrap text-sm font-medium text-gray-900 text-left">
+                  {carModel.seatingCapacity}
+                </td>
+                <td className="px-5 py-4 md:whitespace-nowrap text-sm font-medium text-gray-900 text-left">
+                  {carModel.transmissionType}
+                </td>
+                <td className="px-5 py-4 md:whitespace-nowrap text-right text-sm font-medium">
+                  <button
+                    className="text-red-600 hover:text-red-900"
+                    onClick={() => handleDelete(carModel)}
+                  >
+                    Eliminar
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      {carModelToDelete && (
+        <ConfirmationModal
+          processText={`eliminar el modelo de vehículo "${carModelToDelete.name}"`}
+          onAccept={handleDeleteConfirmation}
+          onCancel={handleDeleteCancel}
+          actionType="delete"
+        />
+      )}
+      <Pagination
+        totalPages={totalPages}
+        currentPage={currentPage}
+        onPageChange={setCurrentPage}
+      />
     </section>
   );
-}
+};
 
-export default ModelTable;
+export default CarModelList;
