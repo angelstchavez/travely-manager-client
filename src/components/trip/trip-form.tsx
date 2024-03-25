a17cac4542bde0b6ceb21f2ea31c961fc3d964f3
angelstchavez <angelstchavez@gmail.com>
2024-03-24 23:03:51 -0500

    anda

--- src/components/trip/trip-form.tsx
+++ src/components/trip/trip-form.tsx
@@ -1,12 +1,12 @@
+"use client";
+
 import React from "react";
 
 function TripForm() {
   return (
     <>
       <section className="border rounded p-4 my-4 bg-white">
-      <h2 className="text-lg font-semibold">
-        Registrar viaje
-      </h2>
+        <h2 className="text-lg font-semibold">Registrar viaje</h2>
       </section>
     </>
   );
