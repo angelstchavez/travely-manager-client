0658c26dfbf4b0de5f76ca4ca531a9d2595d6103
angelstchavez <angelstchavez@gmail.com>
2024-03-26 21:28:54 -0500

    feat: update

--- src/components/sale/sale-register.tsx
+++ src/components/sale/sale-register.tsx
@@ -3,8 +3,7 @@ import Bus from "../utils/sale/bus";
 import SeatStatusCounts from "../utils/sale/seat-counter";
 import TripDetails from "../utils/sale/trip-detail";
 import SelectedSeatsDisplay from "../utils/sale/selected-seat";
-import PassengersSale from "../utils/sale/passengers-sale";
-import PaymentSale from "../utils/sale/payment-sale";
+import ConfirmationModal from "../modals/confirmation-modal"; // Importamos el componente de confirmación
 
 interface SaleRegisterProps {
   tripId: number;
@@ -28,100 +27,82 @@ const SaleRegister: React.FC<SaleRegisterProps> = ({ tripId, onCancel }) => {
     { id: string; number: number }[]
   >([]);
   const [showMainSection, setShowMainSection] = useState(true);
-  const [showConfirmationSection, setShowConfirmationSection] = useState(false);
   const [showPaymentSection, setShowPaymentSection] = useState(false);
   const [passengers, setPassengers] = useState<Passenger[]>([]);
+  const [showCancelConfirmation, setShowCancelConfirmation] = useState(false); // Estado para controlar la visibilidad del modal de confirmación
 
   const handleSelectedSeatsChange = (newSelectedSeat: {
     id: string;
     number: number;
   }) => {
-    setSelectedSeats((prevSelectedSeats) => {
-      const existingIndex = prevSelectedSeats.findIndex(
-        (seat) => seat.id === newSelectedSeat.id
-      );
-      if (existingIndex !== -1) {
-        return prevSelectedSeats.filter((_, index) => index !== existingIndex);
-      } else {
-        return [...prevSelectedSeats, newSelectedSeat];
-      }
-    });
+    if (
+      selectedSeats.length < 5 ||
+      selectedSeats.some((seat) => seat.id === newSelectedSeat.id)
+    ) {
+      setSelectedSeats((prevSelectedSeats) => {
+        const existingIndex = prevSelectedSeats.findIndex(
+          (seat) => seat.id === newSelectedSeat.id
+        );
+        if (existingIndex !== -1) {
+          return prevSelectedSeats.filter(
+            (_, index) => index !== existingIndex
+          );
+        } else {
+          return [...prevSelectedSeats, newSelectedSeat];
+        }
+      });
+    }
   };
 
-  const handleContinueClick = () => {
-    const newPassengers: Passenger[] = selectedSeats.map((seat) => ({
-      name: "",
-      surname: "",
-      documentType: "",
-      documentNumber: "",
-      email: "",
-      seat: { id: seat.id, number: seat.number },
-    }));
-    setPassengers(newPassengers);
-    setShowMainSection(false);
-    setShowConfirmationSection(true);
+  const handleCancel = () => {
+    setShowCancelConfirmation(true); // Mostramos el modal de confirmación cuando se hace clic en cancelar
   };
 
-  const handleReturnClick = () => {
-    setSelectedSeats([]);
-    setShowMainSection(true);
-    setShowConfirmationSection(false);
-    setShowPaymentSection(false);
+  const handleCancelConfirmed = () => {
+    onCancel(); // Llamamos a la función onCancel cuando se confirma la cancelación
+    setShowCancelConfirmation(false); // Ocultamos el modal de confirmación después de confirmar
   };
 
-  const handlePaymentClick = () => {
-    setShowConfirmationSection(false);
-    setShowPaymentSection(true);
+  const handleCancelModalClose = () => {
+    setShowCancelConfirmation(false); // Ocultamos el modal de confirmación si se cancela
   };
 
   return (
     <>
       {showMainSection && (
-        <div className="flex flex-col sm:flex-row gap-2 p-2">
-          <div className="overflow-hidden md:w-[300px] lg:w-[300px]">
-            <SeatStatusCounts tripId={tripId} />
-            <Bus
-              tripId={tripId}
-              onSelectedSeatsChange={handleSelectedSeatsChange}
-            />
-          </div>
-          <div className="overflow-hidden flex-grow">
-            <TripDetails tripId={tripId} />
-            {selectedSeats.map((seat, index) => (
-              <PassengersSale
-                key={index}
-                seatId={seat.id}
-                seatNumber={seat.number}
-                selectedSeat={seat}
+        <div className="flex justify-center items-center p-2">
+          <div className="flex flex-col sm:flex-row gap-2 p-2">
+            <div className="overflow-hidden md:w-[300px] lg:w-[300px]">
+              <SeatStatusCounts tripId={tripId} />
+              <Bus
+                tripId={tripId}
+                onSelectedSeatsChange={handleSelectedSeatsChange}
               />
-            ))}
-          </div>
-          <div className="tm-1 space-x-2">
-            <button
-              onClick={handleReturnClick}
-              className="bg-zinc-100 hover:bg-zinc-200 text-zinc-700 font-bold py-2 px-4 rounded"
-            >
-              Regresar
-            </button>
-          </div>
-          <div className="space-x-2">
-            <button
-              onClick={handlePaymentClick}
-              className="bg-green-700 text-white font-bold py-2 px-4 rounded"
-            >
-              Pagar
-            </button>
+            </div>
+            <div className="overflow-hidden flex-grow">
+              <TripDetails tripId={tripId} />
+              <div className="overflow-hidden flex-grow">
+                <SelectedSeatsDisplay selectedSeatIds={selectedSeats} />
+              </div>
+              <div className="mt-2">
+                <button
+                  onClick={handleCancel}
+                  className="bg-zinc-200 p-1 px-3 rounded border"
+                >
+                  Cancelar
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       )}
-
-      {showPaymentSection && (
-        <PaymentSale
-          passengers={passengers}
-          onCancel={() => {
-            setShowMainSection(true);
-            setShowPaymentSection(false);
-          }}
+      {/* Mostramos el modal de confirmación si showCancelConfirmation es verdadero */}
+      {showCancelConfirmation && (
+        <ConfirmationModal
+          processText="cancelar la compra"
+          onAccept={handleCancelConfirmed}
+          onCancel={handleCancelModalClose}
+          actionType="register"
         />
       )}
     </>
--- src/components/utils/sale/selected-seat.tsx
+++ src/components/utils/sale/selected-seat.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface SelectedSeatsDisplayProps {
   selectedSeatIds: { id: string; number: number }[];
@@ -9,20 +9,30 @@ const SelectedSeatsDisplay: React.FC<SelectedSeatsDisplayProps> = ({
 }) => {
   return (
     <div className="mt-2 bg-white rounded border p-1 overflow-auto">
-      <h2 className="px-1 font-semibold">Asientos seleccionados</h2>
+      <h2 className="px-1 font-semibold bg-tm20 rounded text-center p-1 text-white">
+        Asientos seleccionados
+      </h2>
       <div className="flex flex-wrap">
-        {selectedSeatIds.map((seat) => (
-          <div key={seat.id} className="m-1">
-            <div
-              className={` border border-gray-300 rounded-md flex items-center justify-center transition-colors duration-200 bg-green-600 hover:bg-green-500`}
-              style={{ width: "50px", height: "40px" }}
-            >
-              <span className={`font-bold text-xm text-white`}>
-                {seat.number}
-              </span>
-            </div>
+        {selectedSeatIds.length === 0 ? (
+          <div className="m-1">
+            <p className="text-center text-gray-500">
+              No hay asientos seleccionados
+            </p>
           </div>
-        ))}
+        ) : (
+          selectedSeatIds.map((seat) => (
+            <div key={seat.id} className="m-1">
+              <div
+                className={` border border-gray-300 rounded-md flex items-center justify-center transition-colors duration-200 bg-green-600 hover:bg-green-500`}
+                style={{ width: "40px", height: "40px" }}
+              >
+                <span className={`font-bold text-xm text-white`}>
+                  {seat.number}
+                </span>
+              </div>
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
